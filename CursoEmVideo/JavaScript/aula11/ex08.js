744abb9b6222c747643285c2847996622c1316ed
Andre Gomes <andregomes4@hotmail.com>
2023-09-24 01:41:05 -0300

    Aulas 09 (revisão), 10 e 11 de JS do Curso em Vídeo

---
+++ CursoEmVideo/JavaScript/aula11/ex08.js
@@ -0,0 +1,7 @@
+var vel = 69
+console.log (`A velocidade do seu carro é ${vel}Km/h...`)
+if (vel > 60){
+    console.log (`Sua velocidade foi de ${vel} Km/h... Você foi multado!`)
+} else{
+    console.log ('Dirija sempre usando o cinto de segurança!')
+}
---
+++ CursoEmVideo/JavaScript/aula11/ex09.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log (`Você está morando em ${pais}...`)
+if (pais != 'Brasil'){
+    console.log ('Vocé é estrangeiro!')
+} else{
+    console.log ('Você é brasileiro!')
+}
